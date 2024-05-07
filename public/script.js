ae500601126e9a292d4ffbcf97a36dbf82721312
kimdiep03 <162079945+kimdiep03@users.noreply.github.com>
2024-05-07 23:24:57 +1000

    update

--- public/script.js
+++ public/script.js
@@ -8,7 +8,17 @@ canvas.height = window.innerHeight;
 console.log(ctx);
 
 class Particle {
-
+   constructor(effect) {
+      this.effect = effect;
+      this.x = Math.random() * this.effect.width;
+      this.y = Math.random() * this.effect.height;
+      this.radius = 15;
+   }
+   draw(context){
+      context.beginPath();
+      context.arc(this.x, this.y, this.radius, 0, Math.PI *2);
+      context.fill();
+   }
 }
 
 class Effect {
@@ -16,9 +26,25 @@ class Effect {
       this.canvas = canvas;
       this.width = this.canvas.width;
       this.height = this.canvas.height;
+      this.particles = [];
+      this.numberOfParticles = 20;
+      this.createParticles();
+   }
+   createParticles(){
+      for (let i = 0; i < this.numberOfParticles; i++) {
+         this.particles.push(new Particle(this));
+      }
+   }
+   handleParticles(){
+      this.particles.forEach(particle => {
+         particle.draw(context);
+      })
    }
 
 }
+
+const effect = new Effect(canvas);
+console.log(effect);
 
 function animate() {
 
