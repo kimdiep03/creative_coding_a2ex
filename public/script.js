810ebafeb9c99d932848d791ea819602c6790fe9
kimdiep03 <162079945+kimdiep03@users.noreply.github.com>
2024-05-07 11:12:02 +1000

    update

--- public/script.js
+++ public/script.js
@@ -2,8 +2,8 @@ document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 
 const cnv = document.getElementById (`cnv_element`)
-cnv.width = innerWidth
-cnv.height = innerHeight
+cnv.width = window.innerWidth
+cnv.height = window.innerHeight
 
 const ctx = cnv.getContext (`2d`)
 
@@ -12,6 +12,9 @@ const draw_frame = () => {
    ctx.fillRect (0, 0, innerWidth, innerHeight)
 
    requestAnimationFrame (draw_frame)
+
+   ctx.fillStyle = 'white';
+   ctx.fillRect(250, 150, 100, 200);
 }
 
 draw_frame ()
@@ -19,4 +22,16 @@ draw_frame ()
 window.onresize = () => {
    cnv.width = innerWidth
    cnv.height = innerHeight   
+}
+
+class Particle {
+
+}
+
+class Effect {
+
+}
+
+function animate() {
+
 }
