9be3dc003e872019a245da63a78e9269751411c1
kimdiep03 <162079945+kimdiep03@users.noreply.github.com>
2024-05-09 17:43:29 +1000

    update

--- public/script.js
+++ public/script.js
@@ -102,7 +102,7 @@ class Root {
    }
 
 
-window.addEventListener('mousemove', function(e){
+window.addEventListener('mousemove', function(e)){
          const root = new Root(e.clientX, e.clientY, getRandomHSLColor()); // 
          root.update(); // context to the update method
 
